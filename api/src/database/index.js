249da2b175d8762ab3e827dbbcba6637b8d97b38
Saullo Reis <saulloreis01@hotmail.com>
2023-05-15 12:00:52 -0300

    adicionando a tabela posts

--- api/src/database/index.js
+++ api/src/database/index.js
@@ -1,21 +1,30 @@
 import sqlite3 from "sqlite3";
 
 const db = new sqlite3.Database('database.db', (err) => {
-    if(err) {
+    if (err) {
         console.error(err.message)
     }
     console.log('Conectado ao banco de dados')
 })
 
 db.run(`CREATE TABLE IF NOT EXISTS users (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name TEXT NOT NULL UNIQUE,
-  email TEXT NOT NULL UNIQUE,
-  password TEXT NOT NULL
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL UNIQUE,
+    email TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL
 )`, (err) => {
-    if(err){
-        console.error(err.message)
-    }
+    if (err) return console.error(err.message)
+
+})
+
+db.run(`CREATE TABLE IF NOT EXISTS posts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    photo TEXT NOT NULL,
+    content TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)`, (err) => {
+    if (err) return console.error(err.message)
 })
 
 export { db }
