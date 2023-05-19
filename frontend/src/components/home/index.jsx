8011f7b74e7e90268eeb2ce5e2748efbd0e16dc2
Saullo Reis <saulloreis01@hotmail.com>
2023-05-19 11:42:05 -0300

    modificado estilos

--- frontend/src/components/home/index.jsx
+++ frontend/src/components/home/index.jsx
@@ -2,12 +2,13 @@ import { Footer } from "../headerAndFooter/footer/Footer"
 import { Header } from "../headerAndFooter/header/Header"
 import { Perfil } from "./perfil"
 import { Posts } from "./posts"
+import './style.sass'
 
 export const Home = () => {
     return(
         <section className="home" >
             <Header/>
-            <main style={{ minHeight: '88vh', display: 'flex' }}>
+            <main className="container">
                 <Perfil/>
                 <Posts />
             </main>
