9a98a37063164b2bdb9a23ccf61894ed97d2ae0c
Saullo Reis <saulloreis01@hotmail.com>
2023-05-26 11:37:57 -0300

    :sparkles: feat: Adicionando o sha256

--- api/src/controllers/users.js
+++ api/src/controllers/users.js
@@ -17,9 +17,7 @@ const login = (req, res) => {
     const q = 'SELECT id, email, password, photo, name FROM users WHERE email = ?'
     const { email, password } = req.body
 
-    console.log(sha256(password))
     db.get(q, email, function (err, user) {
-        console.log(user)
         if (err) return res.status(500).json({ error: 'Erro no servidor' })
         if (!user || user.password !== sha256(password)) return res.status(401).json({ error: 'Credenciais incorretos' })
         return res.status(200).json(user);
