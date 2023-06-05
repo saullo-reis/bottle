d3615398cd0ba9abb4fb7202e452df13b2843012
Saullo Reis <saulloreis01@hotmail.com>
2023-06-05 11:45:03 -0300

    feat : Criando o componente de notifications

---
+++ frontend/src/components/notification/index.jsx
@@ -0,0 +1,46 @@
+import { Footer } from "../headerAndFooter/footer/Footer"
+import { Header } from "../headerAndFooter/header/Header"
+import { Perfil } from "../main/user/index"
+import { FollowAdd } from "../follows/Follows"
+import { styled } from "styled-components"
+import { Notification } from "./notification"
+
+export const Notifications = () => {
+    return (
+        <MainStyle>
+            <Header />
+            <main>
+                <Perfil />
+                <Notification/>
+                <FollowAdd />
+            </main>
+            <Footer />
+        </MainStyle>
+    )
+}
+
+const MainStyle = styled.section`
+    display: flex;
+    justify-content: space-between;
+    flex-direction: column;
+    align-items: center;
+    background-color: #2F2E2E;
+    min-height: 100vh;
+    main{
+        min-height: 88vh;
+        display: flex;
+        justify-content: space-between;
+        width: 93vw;
+    }
+
+    @media ( max-width: 800px ){
+        main{
+            width: 100vw;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+        }  
+    }      
+    
+`
